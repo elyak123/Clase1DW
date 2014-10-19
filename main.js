f3f00c8c96d5926f0b2ffd8fea299c27ccaf18f5
elyak123 <elyak.123@gmail.com>
2014-10-19 09:16:19 -0500

    Pulse 2014 agrega nuevo post

---
+++ main.js
@@ -0,0 +1,27 @@
+var $form = $('#formulario'),
+	$titulo = $('#titulo'),
+	$url = $('#url'),
+	$publicar = $('#mostrar-form'),
+	$list = $('#contenido'),
+	$agregar = $('#agregar'),
+	$post = $('.item').first();
+
+	function mostrarFormulario(){
+		$form.slideToggle();
+		return false;
+	}
+	function agregarPost (){
+		var url = $url.val(),
+			titulo = $titulo.val(),
+			$clone = $post.clone();
+		$clone.find('.Article-title').text(titulo);
+		$clone.find('.tag_item').attr('href', url);
+
+		$clone.hide();
+		$list.prepend($clone);
+		$clone.fadeIn();
+	}
+
+	//eventos
+	$publicar.click(mostrarFormulario);
+	$form.on('submit', agregarPost);
