e38fa9007ecf79a94c69bf8699ad22e45ed93128
elyak123 <elyak.123@gmail.com>
2014-10-26 06:06:11 -0600

    pequeño error de sintaxis

--- geo.js
+++ geo.js
@@ -7,7 +7,7 @@ function geo_error(){
 
 function geo_exito(posicion){
 	var lat = posicion.coords.latitude;
-	var lon = posicion.corrds.longitude;
+	var lon = posicion.cords.longitude;
 	var mapa = new Image();
 	mapa.src = "http//maps.google.com/maps/apu/staticmap?"
 }
