2f6b431b96eff19d56a0b5dce62d90572758afe2
elyak123 <elyak.123@gmail.com>
2014-10-26 06:54:52 -0600

    veremos si el mapa se visualiza en la consola.

--- geo.js
+++ geo.js
@@ -9,7 +9,7 @@ function geo_exito(posicion){
 	var lat = posicion.coords.latitude;
 	var lon = posicion.coords.longitude;
 	var mapa = new Image();
-	mapa.src = "http//maps.google.com/maps/api/staticmap?zoom=13&size=200x200&sensor=false&center="+lat+","+lon;
+	mapa.src = "http://maps.googleapis.com/maps/api/staticmap?zoom=13&size=200x200&sensor=false&center="+lat+","+lon;
 }
 
 geo.getCurrentPosition(geo_exito, geo_error, opciones);
